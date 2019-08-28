964cdfc2cb085643fb815b6576dffb9fffb826ea
Harrison Cole <harrison.d.cole@gmail.com>
2019-08-28 15:59:09 -0400

    background set to body, not div

---
+++ main.js
@@ -0,0 +1,20 @@
+function projectBarFncGenerator() {
+  let isDisplayed = false;
+  const projectBar = document.createElement('div');
+  projectBar.className = 'project-bar';
+  projectBar.innerHTML =
+    '<a href="www.test.com">DiveBook</a><a href="www.test.com">What To Wear</a><a href="www.test.com">Grace Shredder</a>';
+  const projects = document.querySelector('.container-home');
+  function createProjectBar() {
+    !isDisplayed
+      ? projects.appendChild(projectBar)
+      : projects.removeChild(projectBar);
+    isDisplayed = !isDisplayed;
+  }
+  return createProjectBar;
+}
+
+// const createProjectBar = projectBarFncGenerator();
+// const projects = document.getElementById('projects');
+
+// projects.addEventListener('click', createProjectBar);
